9d72cae99528c8c64f4dbf05f65c90f406734257
coneapps <33197871+coneapps@users.noreply.github.com>
2017-10-30 11:27:59 +0300

    Update SelectLevel.cs

--- SelectLevel.cs
+++ SelectLevel.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Created by aliihsan.
@@ -19,7 +20,7 @@ public class SelectLevel : MonoBehaviour {
 		
 	}
 
-	public void loadLevel(int level){
-		Application.LoadLevel ("");  //Send level info
+	public void loadLevel(string level){
+		SceneManager.LoadScene("");  //Send level info
 	}
 }
