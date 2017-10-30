8607ec966ea2d5ac139de3379f5e66d30ead48a6
coneapps <33197871+coneapps@users.noreply.github.com>
2017-10-30 11:26:55 +0300

    Update SelectCategory.cs

--- SelectCategory.cs
+++ SelectCategory.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Created by aliihsan.
@@ -9,7 +10,6 @@ using UnityEngine;
 //TODO:Ekrana kategorileri çizdir, eventlarını yaz, loadCategory metodunda ilgili parametreyi selectLevel scene'ine geçir
 
 public class SelectCategory : MonoBehaviour {
-
 	void Start () {
 		
 	}
@@ -20,11 +20,14 @@ public class SelectCategory : MonoBehaviour {
 
 	void OnGUI(){
 
-
 	}
 
-	public void loadCategory(int catId){
-		Application.LoadLevel ("");  //Send catId
+	public void loadCategory(string category){
+        SceneManager.LoadScene(""); // Send catId
 	}
 
+    public void goBack()
+    {
+        SceneManager.LoadScene("menu");
+    }
 }
