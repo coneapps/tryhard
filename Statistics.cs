e47fe9beff549653bd9350cc184765a028041106
ahmsay <ahmsay@gmail.com>
2017-11-13 11:32:14 +0300

    Update Statistics.cs

--- Statistics.cs
+++ Statistics.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// Created by aliihsan.
@@ -10,13 +11,15 @@ using UnityEngine.SceneManagement;
 //TODO:Statistics scene'ini burda oluştur.
 
 public class Statistics : MonoBehaviour {
+    public GameObject message;
 
 	void Start () {
-		
-	}
-
-	void Update () {
-		
+        int IScore = PlayerPrefs.GetInt("iScore");
+        int TapScore = PlayerPrefs.GetInt("tapScore");
+        int TypeScore = PlayerPrefs.GetInt("typeScore");
+        int WordScore = PlayerPrefs.GetInt("wordScore");
+        int VisualScore = PlayerPrefs.GetInt("visualScore");
+        message.GetComponent<Text>().text = "Best scores\nIntelligence: " + IScore + "\nTapping: " + TapScore + "\nTyping: " + TypeScore + "\nWord Memory: " + WordScore + "\nVisual Memory: " + VisualScore;
 	}
 
     public void goBack()
