d9a34a2fd5b5ea7217f6517f4173ecc2a441790f
ahmsay <ahmsay@gmail.com>
2017-11-13 11:30:50 +0300

    Update SceneLoader.cs

--- SceneLoader.cs
+++ SceneLoader.cs
@@ -10,20 +10,141 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour {
 
-	level1 lvl1 = new level1 ();
+	public static int catId = -1;  //Default = -1 for null value
+
+
+	public string[] intelligenceInfos;
+	public string[] tappingInfos;
+	public string[] typingInfos;
+	public string[] wordInfos;
+	public string[] visualInfos;
+
+	public GameObject[] intelligenceLevels;
+	public GameObject[] tappingLevels;
+	public GameObject[] typingLevels;
+	public GameObject[] wordMemoryLevels;
+	public GameObject[] visualMemoryLevels;
+
+	public GameObject infoUI;
+	public GameObject endUI;
+
+	private IGameScene gameScene;
+
+	private int iScore;
+	private int typeScore;
+	private int tappScore;
+	private int wordScore;
+	private int visualScore;
 
 	void Start () {
-		lvl1.Start ();
+		
+		switch (catId) {
+		case -1:
+			Application.Quit ();
+			break;
+		case 0:
+			//gameScene = new Intelligence ();
+			intelligenceLevels[0].SetActive(true);
+			break;
+		case 1:
+			gameScene = new Typing (this);
+			break;
+		case 2:
+			gameScene = new Tapping (this);
+			break;
+		case 3:
+			gameScene = new Word (this);
+			break;
+		case 4:
+			gameScene = new Visual (this);
+			break;
+		}
+
+		if (gameScene != null) {
+			gameScene.Start ();
+		}
 	}
 
 	void Update () {
-		lvl1.Update ();
-		
+		if (gameScene != null) {
+			gameScene.Update ();
+		}
+
+
 	}
 
 	void OnGUI(){
-		lvl1.OnGUI ();
+		if (gameScene != null) {
+			gameScene.OnGUI ();
+		}
+
 
 	}
+
+	#region Sends data to gameScene class(tapping)
+	public void increaseTouchCounter(){
+		(gameScene as Tapping).touchCounter++;
+	}
+
+
+	#endregion
+
+	#region Sends data to gameScene class(typing)
+	public void addLetter(string c){
+		(gameScene as Typing).addLetter (c);
+	}
+
+	public void deleteLetter(){
+		(gameScene as Typing).deleteLetter ();
+	}
+	#endregion
+
+
+	public void nextLvl(){
+		gameScene.nextButtonClicked ();
+	}
+
+
+	#region Score Management methods
+	public void addIntelligenceScore(int score){
+		iScore += score;
+	}
+	public void addTappingScore(int score){
+		tappScore += score;
+	}
+	public void addTypingScore(int score){
+		typeScore += score;
+	}
+	public void addWordScore(int score){
+		wordScore += score;
+	}
+	public void addVisualScore(int score){
+		visualScore += score;
+	}
+
+	public void saveScores(){
+		int prevIScore = PlayerPrefs.GetInt ("iScore");
+		int prevTapScore = PlayerPrefs.GetInt ("tapScore");
+		int prevTypeScore = PlayerPrefs.GetInt ("typeScore");
+		int prevWordScore = PlayerPrefs.GetInt ("wordScore");
+		int prevVisualScore = PlayerPrefs.GetInt ("visualScore");
+
+		if (iScore > prevIScore) {
+			PlayerPrefs.SetInt ("iScore",iScore);
+		}
+		if (tappScore > prevTapScore) {
+			PlayerPrefs.SetInt ("tapScore",tappScore);
+		}
+		if (typeScore > prevTypeScore) {
+			PlayerPrefs.SetInt ("typeScore",typeScore);
+		}
+		if (wordScore > prevWordScore) {
+			PlayerPrefs.SetInt ("wordScore",wordScore);
+		}
+		if (visualScore > prevVisualScore) {
+			PlayerPrefs.SetInt ("visualScore",visualScore);
+		}
+	}
+	#endregion
 
 }
