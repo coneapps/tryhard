b1a6cd37c4990729585551b5048870b24b6f0470
coneapps <33197871+coneapps@users.noreply.github.com>
2017-12-17 21:47:02 +0300

    Add files via upload

--- Menu.cs
+++ Menu.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,16 +10,47 @@ using UnityEngine.SceneManagement;
 //TODO:btn... methodlarını doldur, başlangıçta butonları ekrana çizdir ve eventlarını yaz.
 
 public class Menu : MonoBehaviour {
+
+	public TextAsset textAsset;
+
+    GPGS gpgs = new GPGS();
+    public GameObject settingsButtons;
+    public GameObject signed;
+    public GameObject unsigned;
+    public GameObject tur;
+    public GameObject eng;
+    public GameObject muted;
+    public GameObject unmuted;
+
     public void Authenticate()
     {
-        // Sign in / out
+        if (gpgs.isSigned())
+        {
+            signed.SetActive(true);
+            unsigned.SetActive(false);
+        }
+        else
+        {
+            signed.SetActive(false);
+            unsigned.SetActive(true);
+        }
+        gpgs.sign();
     }
 	void Start () {
-		
+        if (Lang.lang == 0)
+            tur.SetActive(false);
+        else
+            eng.SetActive(false);
+
+        if (gpgs.isSigned())
+            unsigned.SetActive(false);
+        else
+            signed.SetActive(false);
 	}
 
 	void Update () {
-			
+        if (Input.GetKeyDown(KeyCode.Escape))
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
 	}
 		
 	public void OnGUI(){
@@ -27,26 +58,51 @@ public class Menu : MonoBehaviour {
 	}
 
 	public void btnPlay(){
-		SceneManager.LoadScene("SelectCategory");
+		SceneManager.LoadScene("selectCategory");
 	}
 
 	public void btnHow2Play(){
-		SceneManager.LoadScene("HowToPlay");
+		SceneManager.LoadScene("howToPlay");
 	}
 
 	public void btnAchievements(){
-
+        gpgs.showAchievements();
 	}
 
 	public void btnLeaderBoards(){
-
+        gpgs.showLeaderboards();
 	}
 
 	public void btnStatistics(){
-		SceneManager.LoadScene("Statistics");
+		SceneManager.LoadScene("statistics");
 	}
 
-	public void btnExit(){
-		Application.Quit ();
+    public void btnSettings()
+    {
+		if (!settingsButtons.activeSelf)
+			settingsButtons.SetActive (true);
+        else 
+			settingsButtons.SetActive (false);
 	}
+
+	public void changeLanguage()
+    {
+		if (PlayerPrefs.GetString("language").Equals("English")) 
+        {
+            Lang.setLanguage("Turkish", textAsset);
+            tur.SetActive(true);
+            eng.SetActive(false);
+        }
+        else
+        {
+            Lang.setLanguage("English", textAsset);
+            tur.SetActive(false);
+            eng.SetActive(true);
+        }
+    }
+
+    public void btnVolume()
+    {
+
+    }
 }
