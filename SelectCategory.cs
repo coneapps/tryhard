86885fdf25388fbaf6035bb084594062cfb11475
coneapps <33197871+coneapps@users.noreply.github.com>
2017-12-17 21:47:25 +0300

    Add files via upload

--- SelectCategory.cs
+++ SelectCategory.cs
@@ -1,33 +1,44 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// Created by aliihsan.
 /// </summary>
 
-//TODO:Ekrana kategorileri çizdir, eventlarını yaz, loadCategory metodunda ilgili parametreyi selectLevel scene'ine geçir
-
 public class SelectCategory : MonoBehaviour {
+
+    public GameObject cat1;
+    public GameObject cat2;
+    public GameObject cat3;
+    public GameObject cat4;
+    public GameObject cat5;
+
 	void Start () {
-		
+        cat1.GetComponent<Text>().text = Lang.getString("cat1");
+        cat2.GetComponent<Text>().text = Lang.getString("cat2");
+        cat3.GetComponent<Text>().text = Lang.getString("cat3");
+        cat4.GetComponent<Text>().text = Lang.getString("cat4");
+        cat5.GetComponent<Text>().text = Lang.getString("cat5");
 	}
 
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Escape))
+            goBack();
 	}
 
-	void OnGUI(){
+	public void loadGameScene(int num)
+    {
+		SceneLoader.catId = num;
+        SceneManager.LoadScene("gameScene");
+    }
 
-	}
-
-	public void loadCategory(string category){
-        SceneManager.LoadScene(""); // Send catId
-	}
 
     public void goBack()
     {
-        SceneManager.LoadScene("menu");
+		SceneManager.LoadScene("menu");
     }
+
 }
