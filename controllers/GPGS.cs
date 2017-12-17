2163be522b83104233256c81412019656f5400a7
coneapps <33197871+coneapps@users.noreply.github.com>
2017-12-17 21:50:08 +0300

    Add files via upload

---
+++ controllers/GPGS.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi;
+
+public class GPGS : MonoBehaviour {
+
+    public void Start()
+    {
+        PlayGamesPlatform.Activate();
+    }
+
+    public void sign()
+    {
+        if (isSigned())
+        {
+            PlayGamesPlatform.Instance.SignOut();
+        }
+        else
+        {
+            Social.localUser.Authenticate((bool success) => { Debug.Log(success); });
+        }
+    }
+
+    public void showAchievements()
+    {
+        Social.ShowAchievementsUI();
+    }
+
+    public void showLeaderboards()
+    {
+        Social.ShowLeaderboardUI();
+    }
+
+    public void load(string leaderboardID, string scoreType)
+    {
+        PlayGamesPlatform.Instance.LoadScores(leaderboardID, LeaderboardStart.PlayerCentered, 1, LeaderboardCollection.Public, LeaderboardTimeSpan.AllTime, (LeaderboardScoreData data) =>
+        {
+            if (data.Valid)
+            {
+                int score = int.Parse(data.PlayerScore.formattedValue);
+                PlayerPrefs.SetInt(scoreType, score);
+            }
+        });
+    }
+
+    public void unlockAchivement(string id)
+    {
+        Social.ReportProgress(id, 100.0f, (bool success) => { });
+    }
+
+    public void postScore(int score, string id)
+    {
+        Social.ReportScore(score, id, (bool success) => { });
+    }
+
+    public bool isSigned()
+    {
+        return Social.localUser.authenticated;
+    }
+}
