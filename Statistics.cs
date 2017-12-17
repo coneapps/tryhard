c09d3e9850757ba4f1776e732272c9f96e3e79f6
coneapps <33197871+coneapps@users.noreply.github.com>
2017-12-17 21:47:38 +0300

    Add files via upload

--- Statistics.cs
+++ Statistics.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,15 +12,36 @@ using UnityEngine.UI;
 
 public class Statistics : MonoBehaviour {
     public GameObject message;
+    GPGS gpgs = new GPGS();
+    int IScore;
+    int TapScore;
+    int TypeScore;
+    int WordScore;
+    int VisualScore;
 
-	void Start () {
-        int IScore = PlayerPrefs.GetInt("iScore");
-        int TapScore = PlayerPrefs.GetInt("tapScore");
-        int TypeScore = PlayerPrefs.GetInt("typeScore");
-        int WordScore = PlayerPrefs.GetInt("wordScore");
-        int VisualScore = PlayerPrefs.GetInt("visualScore");
-        message.GetComponent<Text>().text = "Best scores\nIntelligence: " + IScore + "\nTapping: " + TapScore + "\nTyping: " + TypeScore + "\nWord Memory: " + WordScore + "\nVisual Memory: " + VisualScore;
+	void Start() 
+    {
+        if (gpgs.isSigned())
+        {
+            gpgs.load("CgkIm6CK7OEHEAIQAQ", "iScore");
+            gpgs.load("CgkIm6CK7OEHEAIQCA", "typeScore");
+            gpgs.load("CgkIm6CK7OEHEAIQDA", "tapScore");
+            gpgs.load("CgkIm6CK7OEHEAIQEA", "wordScore");
+            gpgs.load("CgkIm6CK7OEHEAIQEQ", "visualScore");
+        }
 	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            goBack();
+        IScore = PlayerPrefs.GetInt("iScore");
+        TapScore = PlayerPrefs.GetInt("tapScore");
+        TypeScore = PlayerPrefs.GetInt("typeScore");
+        WordScore = PlayerPrefs.GetInt("wordScore");
+        VisualScore = PlayerPrefs.GetInt("visualScore");
+        message.GetComponent<Text>().text = Lang.getString("line1") + "\n" + Lang.getString("line2") + IScore + "\n" + Lang.getString("line3") + TypeScore + "\n" + Lang.getString("line4") + TapScore + "\n" + Lang.getString("line5") + WordScore + "\n" + Lang.getString("line6") + VisualScore;
+    }
 
     public void goBack()
     {
