1f9455b8dc915f41020a60fceedcb666917698c5
coneapps <33197871+coneapps@users.noreply.github.com>
2017-12-17 21:46:50 +0300

    Add files via upload

--- Introduction.cs
+++ Introduction.cs
@@ -1,7 +1,9 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// Created by aliihsan.
@@ -10,7 +12,24 @@ using UnityEngine.SceneManagement;
 //TODO:Introda bir süre bekle ve beklerken iconu döndür...
 
 public class Introduction : MonoBehaviour {
+
+    public GameObject exceptionMessage;
+	public TextAsset textAsset;
+    GPGS gpgs = new GPGS();
+
 	void Start () {
+        //Initiating Xml Multi-Lang Class
+        try
+        {
+            Lang.initiate(textAsset);
+        }
+        catch (Exception ex)
+        {
+            exceptionMessage.GetComponent<Text>().text = ex.GetType() + ex.Message;
+        }
+
+        gpgs.Start();
+        gpgs.sign();
         StartCoroutine(waitSeconds());
 	}
 
