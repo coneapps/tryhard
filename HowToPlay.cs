9bf39517bc4f849eb861c513ef55104c95a4b134
coneapps <33197871+coneapps@users.noreply.github.com>
2017-12-17 21:46:26 +0300

    Add files via upload

--- HowToPlay.cs
+++ HowToPlay.cs
@@ -1,7 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// Created by aliihsan.
@@ -10,13 +11,36 @@ using UnityEngine.SceneManagement;
 //TODO:Gerekli arayüzü ekrana çizdir.
 
 public class HowToPlay : MonoBehaviour {
+
+    public GameObject title;
+    public GameObject text;
+    int index = 0;
+    string[] texts = { Lang.getString("hwt1"), Lang.getString("hwt2"), Lang.getString("hwt3"), Lang.getString("hwt4"), Lang.getString("hwt5") };
 	void Start () {
-		
+        text.GetComponent<Text>().text = texts[0];
+        title.GetComponent<Text>().text = Lang.getString("howto");
 	}
 
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Escape))
+            goBack();
 	}
+
+    public void prev()
+    {
+        index--;
+        if (index == -1)
+            index = texts.Length - 1;
+        text.GetComponent<Text>().text = texts[index];
+    }
+
+    public void next()
+    {
+        index++;
+        if (index == texts.Length)
+            index = 0;
+        text.GetComponent<Text>().text = texts[index];
+    }
 
     public void goBack()
     {
