738023cb6caf0516a93b78ee4672448d1146802c
coneapps <33197871+coneapps@users.noreply.github.com>
2017-12-24 21:54:38 +0300

    Add files via upload

---
+++ sprite/rotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+    float speed = 1;
+	void Update () {
+        speed += 0.1f;
+        transform.Rotate(0, 0, speed);
+	}
+}
